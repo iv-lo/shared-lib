43baa628b8b8aec49a9f51d0e4267eeaec229dec
Ilia Lopatin <iv.lopatin@outlook.com>
2024-04-04 18:16:50 +0400

    move class

--- src/org/ucs/BundleHolder.groovy
+++ src/org/ucs/BundleHolder.groovy
@@ -3,6 +3,75 @@ package org.ucs
 import org.ucs.ProjectBundle
 
 class BundleHolder {
+    Map<String, List<ProjectBundle>> bundles = [:]
+
+    BundleHolder() {
+        this.addBundle("Van_1", new ProjectBundle("//vehicle1", "0.0.9.0"))
+        this.addBundle("Van_1", new ProjectBundle("//vehicle2", "0.0.9.0"))
+        
+        this.addBundle("Van_2", new ProjectBundle("//vehicle1", "0.2.0.0"))
+        
+        this.addBundle("Van_3", new ProjectBundle("//vehicle1", "0.13.0.2"))
+        this.addBundle("Van_3", new ProjectBundle("//vehicle2", "0.5.0.1"))
+        this.addBundle("Van_3", new ProjectBundle("//vehicle3", "0.6.0.1"))
+        
+        this.addBundle("Van_4", new ProjectBundle("//vehicle1", "head"))
+        this.addBundle("Van_4", new ProjectBundle("//vehicle2", "head"))
+        this.addBundle("Van_4", new ProjectBundle("//vehicle3", "head"))
+        this.addBundle("Van_4", new ProjectBundle("//vehicle4", "head"))
+        this.addBundle("Van_4", new ProjectBundle("//vehicle5", "head"))
+        
+        this.addBundle("Van_5", new ProjectBundle("//vehicle1", "0.22.0.5"))
+        this.addBundle("Van_5", new ProjectBundle("//vehicle2", "0.22.6.0"))
+    }
+
+    void addBundle(String projectName, ProjectBundle bundle) {
+        if (!bundles.containsKey(projectName)) {
+            bundles[projectName] = []
+        }
+        bundles[projectName].add(bundle)
+    }
+
+
+
+
+
+    // void init() {
+    //     addBundle("Van_1", new ProjectBundle("//vehicle1", "0.0.9.0"))
+    //     addBundle("Van_1", new ProjectBundle("//vehicle2", "0.0.9.0"))
+        
+    //     addBundle("Van_2", new ProjectBundle("//vehicle1", "0.2.0.0"))
+        
+    //     addBundle("Van_3", new ProjectBundle("//vehicle1", "0.13.0.2"))
+    //     addBundle("Van_3", new ProjectBundle("//vehicle2", "0.5.0.1"))
+    //     addBundle("Van_3", new ProjectBundle("//vehicle3", "0.6.0.1"))
+        
+    //     addBundle("Van_4", new ProjectBundle("//vehicle1", "head"))
+    //     addBundle("Van_4", new ProjectBundle("//vehicle2", "head"))
+    //     addBundle("Van_4", new ProjectBundle("//vehicle3", "head"))
+    //     addBundle("Van_4", new ProjectBundle("//vehicle4", "head"))
+    //     addBundle("Van_4", new ProjectBundle("//vehicle5", "head"))
+        
+    //     addBundle("Van_5", new ProjectBundle("//vehicle1", "0.22.0.5"))
+    //     addBundle("Van_5", new ProjectBundle("//vehicle2", "0.22.6.0"))
+    // }
+
+
+    @Override
+    String toString() {
+        bundles.collect { projectName, bundleList ->
+            def bundleStrings = bundleList.collect { bundle ->
+                "${bundle.toString()}"
+            }.join(',\n    ')
+            
+            "\"${projectName}\": [\n    ${bundleStrings}\n]"
+        }.join(",\n") 
+    }
+
+}
+
+
+class BundleHolder {
     private Map<String, List<ProjectBundle>> bundles = [:]
     private boolean initialized = false
 
